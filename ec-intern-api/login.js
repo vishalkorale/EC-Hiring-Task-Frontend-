804791dbe598e3cca0cbc204a46ff88fa398c03f
vishal <koralevishal26@gmail.com>
2024-08-06 13:11:02 +0530

    updated login

--- ec-intern-api/login.js
+++ ec-intern-api/login.js
@@ -4,30 +4,29 @@ import  jwt from 'jsonwebtoken';
 const app = express();
 const port = 3001;
 
-// Middleware to parse JSON bodies
+
 app.use(bodyParser.json());
 
 // Secret key for JWT
 const SECRET_KEY = 'your_secret_key';
 
-// User Login Route
+
 app.post('/auth/login', (req, res) => {
   const { email, password } = req.body;
 
-  // Example logic for user authentication
-  // In a real application, you should verify the email and password with your database
+  
   if (email === 'hi@b68.dev' && password === '12345678') {
-    // Generate a token
+   
     const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: '1h' });
 
-    // Send response with the token
+  
     res.status(200).json({ token });
   } else {
     res.status(401).json({ error: 'Invalid email or password' });
   }
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
